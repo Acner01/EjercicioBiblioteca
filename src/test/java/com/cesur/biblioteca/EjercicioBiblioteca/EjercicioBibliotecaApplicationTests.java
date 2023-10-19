91225306e060ec014fb3bf91d7d76779d61e3624
Acner Mendez Feliz <136130876+Acner01@users.noreply.github.com>
2023-10-19 20:06:03 +0200

    Primer commit del proyecto

---
+++ src/test/java/com/cesur/biblioteca/EjercicioBiblioteca/EjercicioBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesur.biblioteca.EjercicioBiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
